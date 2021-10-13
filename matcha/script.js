b1b3968bd1411e97bb104d11091f9134a399a0cb
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-10-13 00:03:25 -0500

    Modal example for try now button, PLEASE work?

---
+++ matcha/script.js
@@ -0,0 +1,32 @@
+if(document.getElementById("btnModal")){
+    let modal = document.getElementById("Modal");
+    let btn = document.getElementById("btnModal");
+    let span = document.getElementsByClassName("close")[0];
+    let body = document.getElementsByTagName("body")[0];
+
+        btn.onclick = function() {
+        modal.style.display = "block";
+
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+}
