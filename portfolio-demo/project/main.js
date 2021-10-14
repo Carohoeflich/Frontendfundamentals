bce423408331fd6099700e25c51956d714a48b7c
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-10-13 19:10:09 -0500

    Working on a fake portfolio to understand SASS v0.2

--- portfolio-demo/project/main.js
+++ portfolio-demo/project/main.js
@@ -1,16 +1,21 @@
-const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn-burger');
+const menuBtn = document.getElementsByClassName('menu-btn');
+const hamburger = document.getElementsByClassName('menu-btn-burger');
+const nav = document.getElementsByClassName('nav');
+const menuNav = document.getElementsByClassName('menu-nav');
 
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu(){
-if(!showMenu){
+menuBtn.addEventListener('click', () => { if (!showMenu) {
     hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+
     showMenu = true;
-}else{
+} else {
     hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    remove
+
     showMenu = false;
-}
-}
+}});
