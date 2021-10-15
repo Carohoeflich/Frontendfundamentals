15791066f59aa888bacb2258d20c26ac48a0ff11
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-10-15 14:28:56 -0500

    Added js for a welcome email for all forms

--- matcha/script.js
+++ matcha/script.js
@@ -1,3 +1,5 @@
+//Codigo para el que el modal aparezca cuando envias tu email
+
 window.onload = function() {
 
 if(document.getElementById("btnModal")){
@@ -32,4 +34,46 @@ if(document.getElementById("btnModal")){
         }
     }
    }
+}
+
+// Codigo para enviar email de bienvenida
+
+const $forms = document.querySelectorAll(".signup-form");
+const getTemplate = () => {
+    return fetch(".template.html").then((response) => response.text());
+};
+
+const sendEmailToApi = (adress, template) => {
+    fetch(`https://bedu-email-sender-api.herokuapp.com/send`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            adress: adress,
+            template: template,
+        }),
+    })
+        .then((results) => {
+            console.log(results);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
+const sendEmail = (event) =>{
+    event.preventDefault();
+    const email = event.target.querySelector("input").value;
+    getTemplate()
+    .then((template) => {
+        sendEmailToApi(email,template);
+    })
+    .catch((error) => {
+        console.log(error, "Error al convertir el template."):
+    });
+};
+
+for (let i = 0; i < $forms.length; i++){
+    $forms[i].addEventListener("submit", sendEmail);
 }
