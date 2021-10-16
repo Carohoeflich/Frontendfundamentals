ede6740a74200cada17f4f80e8589b7668263953
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-10-15 21:14:42 -0500

    Either the API doesnt work, or the code. But email doesnt send

--- matcha/script.js
+++ matcha/script.js
@@ -39,41 +39,42 @@ if(document.getElementById("btnModal")){
 // Codigo para enviar email de bienvenida
 
 const $forms = document.querySelectorAll(".signup-form");
+
 const getTemplate = () => {
-    return fetch(".template.html").then((response) => response.text());
+  return fetch("./template.html").then((response) => response.text());
 };
 
-const sendEmailToApi = (adress, template) => {
-    fetch(`https://bedu-email-sender-api.herokuapp.com/send`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            adress: adress,
-            template: template,
-        }),
-    })
-        .then((results) => {
-            console.log(results);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-};
-
-const sendEmail = (event) =>{
-    event.preventDefault();
-    const email = event.target.querySelector("input").value;
-    getTemplate()
-    .then((template) => {
-        sendEmailToApi(email,template);
+const sendEmailToApi = (address, template) => {
+  fetch(`https://bedu-email-sender-api.herokuapp.com/send`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      address: address,
+      template: template,
+    }),
+  })
+    .then((results) => {
+      console.log(results);
     })
     .catch((error) => {
-        console.log(error, "Error al convertir el template.");
+      console.error(error);
     });
 };
 
-for (let i = 0; i < $forms.length; i++){
-    $forms[i].addEventListener("submit", sendEmail);
+const sendEmail = (event) => {
+  event.preventDefault();
+  const email = event.target.querySelector("input").value;
+  getTemplate()
+    .then((template) => {
+      sendEmailToApi(email, template);
+    })
+    .catch((error) => {
+      console.log(error, "error al convertir el template.");
+    });
+};
+
+for (let i = 0; i < $forms.length; i++) {
+  $forms[i].addEventListener("submit", sendEmail);
 }
